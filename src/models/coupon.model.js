3057c20b049c37394b649b1d1173845d00e56c5a
Vishwavaghasiya <133637632+Vishwavaghasiya@users.noreply.github.com>
2023-09-03 03:22:56 +0530

    -->> coupon.schema

--- src/models/coupon.model.js
+++ src/models/coupon.model.js
@@ -2,6 +2,36 @@ const mongoose = require('mongoose');
 
 const couponSchema = new mongoose.Schema(
     {
-        
+        code: {
+            type: String,
+            trim: true
+        },
+        discountAmount: {
+            type: Number
+        },
+        //fixed , percentage
+        discountType: {
+            type: String,
+            trim: true
+        },
+        maxUses: {
+            type: Number
+        },
+        usedCount: {
+            type: Number
+        },
+        minPurchaseAmount: {
+            type: Number
+        },
+        conditions: {
+            type: String
+        },
+        is_active: {
+            type: Boolean,
+            default: true
+        }
     }
-)
+);
+
+const Coupon = mongoose.model('coupon', couponSchema);
+module.exports = Coupon
